ca4a3f373ad06d9a9184d68b01185f33f11fc7f0
DanielMelo0 <79672542+DanielMelo0@users.noreply.github.com>
2023-05-01 23:09:10 -0300

    Funções Javascript
    First Commit - DIO.
    
    JS Essentials.

---
+++ Arrays JavaScript/js/script.js
@@ -0,0 +1,117 @@
+/*console.log("Lorem Ipsum");
+
+const alunos = ['João', 'Vitor', 'Marina'];
+
+alunos.push('Renan');
+
+alunos[3] = 'Vinicius';
+
+alunos.push('Teste');
+
+alunos.push(10);
+
+console.log(alunos);
+
+alunos.push(10);
+
+notas.push(5);
+notas.push(7);
+notas.push(8);
+notas.push(2);
+notas.push(5);
+notas.push(8);
+
+const soma = notas[0]+notas[1]+notas[2]+notas[3]+notas[4] + notas[5];*/
+
+/*const soma = notas[0] + notas[1] + notas[2] + notas[3] + notas[4] + notas[5];*/
+ 
+/*
+for (let i = 0; i < notas.length; i++){
+    const element = notas[i];
+    
+for(let i = 0; i < 10; i++){
+    console.log(i);
+}
+
+for(let i = 0; i < nome.length; i++){
+    const element = nome[i];    
+}
+
+}*/
+
+const nome = 'Daniel Arruda Melo';
+console.log(nome);
+
+const notas = [];
+
+notas.push(10);
+notas.push(10);
+notas.push(10);
+
+let soma = 0;
+let number = notas.length; 
+
+for(let i = 0; i < number; i++){
+
+   const nota = notas[i];
+   soma = soma + nota;
+}
+
+
+const media = soma / number;
+console.log(media);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
